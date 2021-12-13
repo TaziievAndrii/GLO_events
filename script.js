b58b046175915cf66e029848545390de2a3fb17a
Taziiev Andrii <andrew13369@icloud.com>
2021-12-13 10:40:12 +0200

    добавил изменения

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const inputText = document.querySelector("#text")
-const square = document.querySelector("#square")
-const e_btn = document.querySelector("#e_btn")
-const range = document.querySelector("#range")
-const circle = document.querySelector("#circle")
-let size;
+const inputText = document.querySelector('#text');
+const square = document.querySelector('#square');
+const eBtn = document.querySelector('#e_btn');
+const btn = document.querySelector('#btn');
+const range = document.querySelector('#range');
+const circle = document.querySelector('#circle');
+const rangeSpan = document.querySelector('#range-span');
 let backgroundColorSquare;
 
+const setColor = function (event) {
+    backgroundColorSquare = event.target.value;
+};
 
-const setColor = function(event) {
-    backgroundColorSquare = event.target.value
-    e_btn.style.display = "block";
-    console.log("backgroundColorSquare",typeof backgroundColorSquare);
-}
-
-const changeStyle = function(){
+const changeStyle = function () {
     square.style.backgroundColor = backgroundColorSquare;
-        e_btn.style.display = "none";
-}
+};
 
-const setSize = function(event){
-circle.style.width = event.target.value + '%'
-circle.style.height = event.target.value + '%'
-}
+const hideBtn = function () {
+    eBtn.style.display = 'none';
+};
 
-inputText.addEventListener('input', setColor)
-e_btn.addEventListener('click', changeStyle)
-range.addEventListener('input', setSize)
+const setSize = function (event) {
+    circle.style.width = event.target.value + '%';
+    circle.style.height = event.target.value + '%';
+    rangeSpan.textContent = event.target.value + '%';
+};
+
+inputText.addEventListener('input', setColor);
+eBtn.addEventListener('click', hideBtn);
+btn.addEventListener('click', changeStyle);
+range.addEventListener('input', setSize);
